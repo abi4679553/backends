2cef8c447b1bc9e7342e251ebddf93d3e8e5ec40
abi4679553 <abivengadajalam7708@gmail.com>
2026-04-13 14:11:14 +0530

    first backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+const port = 4000
+
+const a = 10 
+const b = 20
+const sum = a + b
+console.log(sum);
+
+app.get("/",(req,res)=>{
+    res.send("tha value" + sum);
+})
+
+
+
+app.listen(port, () => {
+    console.log("backend running")
+})
